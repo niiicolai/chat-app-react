dae3bfb66c7fffefd2b3475b39604dacafba1f4c
niiicolai <niiicolai@gmail.com>
2024-09-19 11:15:17 +0200

    move li method its own file

--- src/components/room/RoomList.tsx
+++ src/components/room/RoomList.tsx
@@ -7,44 +7,13 @@ import RoomShow from "./RoomShow";
 import Button from "../utils/Button";
 import Spinner from "../utils/Spinner";
 import Alert from "../utils/Alert";
-import type Room from "../../models/room";
+import RoomListItem from "./RoomListItem";
 
 const RoomList = () => {
   const { room, setRoom } = useRoom();
   const { rooms, error, isLoading } = useRooms();
   const [ createRoom, setCreateRoom ] = useState(false);
   const [ editRoom, setEditRoom ] = useState(null);
-
-  const li = (room: Room) => {
-    return (
-      <li key={room.uuid} className="hover:ring-2 hover:ring-indigo-800 relative rounded-md overflow-hidden">
-        <Button onClick={() => setRoom(room)}
-          button="button"
-          type="primary"
-          slot={
-            <span className="block p-3 text-left">
-              <span className="absolute left-0 right-0 top-0 bottom-0 z-10" 
-                style={{ 
-                  backgroundSize: 'cover',
-                  backgroundPosition: 'center',
-                  backgroundRepeat: 'no-repeat', 
-                  backgroundImage: room.avatar.room_file 
-                    ? `url(${room.avatar.room_file.src})` 
-                    : 'none',
-              }}/>
-              <span className="absolute left-0 right-0 top-0 bottom-0 bg-black/20 z-20"/>
-              <span className="block relative font-bold text-lg z-30">
-                {room.name}
-              </span>
-              <span className="block relative font-bold text-sm z-30">
-                {room.description}
-              </span>
-            </span>
-          }
-        />
-      </li>
-    );
-  }
   
   return (
     <div className="bg-black p-3">
@@ -71,7 +40,7 @@ const RoomList = () => {
                 slot="Create Room"
               />
             </li>
-            {rooms.map((room) => (li(room)))}
+            {rooms.map((room) => (<RoomListItem key={room.uuid} room={room} setRoom={setRoom} />))}
             {!rooms.length && <li className="text-white">No rooms found</li>}
           </ul>
 
---
+++ src/components/room/RoomListItem.tsx
@@ -0,0 +1,36 @@
+import Button from "../utils/Button";
+
+const RoomListItem = (props: any) => {
+    const { room, setRoom } = props;
+
+    return (
+        <li key={room.uuid} className="hover:ring-2 hover:ring-indigo-800 relative rounded-md overflow-hidden">
+            <Button onClick={() => setRoom(room)}
+                button="button"
+                type="primary"
+                slot={
+                    <span className="block p-3 text-left">
+                        <span className="absolute left-0 right-0 top-0 bottom-0 z-10"
+                            style={{
+                                backgroundSize: 'cover',
+                                backgroundPosition: 'center',
+                                backgroundRepeat: 'no-repeat',
+                                backgroundImage: room.avatar.room_file
+                                    ? `url(${room.avatar.room_file.src})`
+                                    : 'none',
+                            }} />
+                        <span className="absolute left-0 right-0 top-0 bottom-0 bg-black/20 z-20" />
+                        <span className="block relative font-bold text-lg z-30">
+                            {room.name}
+                        </span>
+                        <span className="block relative font-bold text-sm z-30">
+                            {room.description}
+                        </span>
+                    </span>
+                }
+            />
+        </li>
+    );
+};
+
+export default RoomListItem;
